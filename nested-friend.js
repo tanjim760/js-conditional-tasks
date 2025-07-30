3527c45b5bd9707efb2576fa280b4005f28e0c47
tanjim760 <tanjimul228@gmail.com>
2025-07-31 00:46:30 +0600

    added nested-friend.js

---
+++ nested-friend.js
@@ -0,0 +1,20 @@
+const myScore = 90;
+const friendScore = 80;
+
+if (myScore > 80) {
+  if (friendScore > 80) {
+    console.log('Go for a lunch')
+  }
+  else if (friendScore < 80 && friendScore >= 60) {
+    console.log('Good luck next time')
+  }
+  else if (friendScore < 60 && friendScore >= 40) {
+    console.log('I will not see his message')
+  }
+  else if (friendScore < 40) {
+    console.log('I will block my friend')
+  }
+}
+else {
+  console.log('I will go to home & sleep. I am feeling depressed')
+}
